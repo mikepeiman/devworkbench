cafaf2cbb3673d107e03a79bd12de94ce6de3942
Mike Peiman <mikepeiman@gmail.com>
2020-10-15 17:11:40 -0400

    Added file/folder sorting and separate presentation. Navigation into folder on click works.

---
+++ src/filesystem/fs.svelte
@@ -0,0 +1,119 @@
+<script>
+  import Nav from "./navigation.svelte";
+  import { onMount } from "svelte";
+
+  const fs = require("fs");
+  const path = require("path");
+
+  let currentFiles = [];
+  let currentDirs = [];
+  $: folderPath = process.cwd();
+  $: root = fs.readdirSync(folderPath);
+  onMount(() => {
+    const dir1 = __dirname;
+    const cwd = process.cwd();
+    console.log("ROOT:", root);
+    console.log("__dirname: ", dir1);
+    console.log("cwd: ", cwd);
+
+    navigate();
+  });
+
+  function navigate() {
+    currentFiles = [];
+    currentDirs = [];
+    fs.readdirSync(folderPath)
+      .map(fileName => {
+        // console.log(`inside folderPath.map: `, fileName);
+        return path.join(folderPath, fileName);
+      })
+      .filter(isFile);
+  }
+
+  const isFile = fileName => {
+    console.log(fs.lstatSync(fileName));
+    if (fs.lstatSync(fileName).isFile()) {
+      currentFiles = [...currentFiles, fileName];
+      // console.log(`currentFiles: `, currentFiles);
+    } else {
+      currentDirs = [...currentDirs, fileName];
+      // console.log(`currentDirs: `, currentDirs);
+    }
+  };
+
+  function fileInfo(e) {
+    console.log(`fileInfo on ${file}: `, file);
+  }
+
+  function navDown(e) {
+    console.log(`navDown clicked here: ${e}`);
+    folderPath = e;
+    let fullPath = `${folderPath}\\${e}`;
+    console.log(`fullpath: ${fullPath}`);
+    navigate();
+  }
+</script>
+
+<style lang="scss">
+  .files-listing {
+    margin-top: 1rem;
+    padding-top: 1rem;
+    border-top: 5px solid rgba(155, 55, 255, 0.7);
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    text-align: left;
+  }
+  .dirs-listing {
+    margin-top: 1rem;
+    padding-top: 1rem;
+    border-top: 5px solid rgba(0, 55, 255, 0.75);
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    text-align: left;
+  }
+  .dir {
+    padding: 1rem;
+    margin: 0.25rem;
+    background: rgba(0, 55, 255, 0.2);
+    width: auto;
+    min-width: 20ch;
+    display: flex;
+    &:hover {
+      background: rgba(0, 55, 255, 0.5);
+      cursor: pointer;
+    }
+  }
+  .file {
+    padding: 0.5rem;
+    margin: 0.25rem;
+    background: rgba(155, 55, 255, 0.3);
+    width: auto;
+    min-width: 20ch;
+    display: flex;
+    &:hover {
+      background: rgba(155, 55, 255, 0.5);
+      cursor: pointer;
+    }
+  }
+</style>
+
+<main>
+  <h1>FS component</h1>
+  <Nav />
+  <h2>DIRECTORIES</h2>
+  <div class="dirs-listing">
+    <!-- {#await currentDirs} -->
+    {#each currentDirs as dir}
+      <div class="dir" on:click={() => navDown(dir)}>{dir}</div>
+    {/each}
+  </div>
+  <h2>FILES</h2>
+  <div class="files-listing">
+    {#each currentFiles as file}
+      <div class="file" on:click={() => fileInfo(file)}>{file}</div>
+    {/each}
+  </div>
+  <!-- {/await} -->
+</main>
---
+++ src/filesystem/navigation.svelte
@@ -0,0 +1,63 @@
+<script>
+// export let newDir
+  const cwd = process.cwd();
+  console.log(`accessing assets: ${cwd}`);
+  function navUp(e) {
+    console.log(`navUp clicked, `, e.target);
+    let newDir = e.target
+  }
+</script>
+
+<style lang="scss">
+  .nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  i {
+    color: #22c5ff;
+    font-size: 2rem;
+
+    /* margin: 1rem; */
+    /* padding: 0.25rem 1rem; */
+    border-radius: 5px;
+  }
+
+  .icon-container {
+    width: auto;
+    height: auto;
+    padding: .5rem 1rem;
+    background: #225599aa;
+    &:hover {
+      background: #22c5ffaa;
+      color: #225599;
+    }
+    display: flex;
+    align-items: center;
+    border-radius: 5px;
+  }
+
+  p {
+    font-size: 1.5rem;
+    margin: .5rem .25rem;
+  padding: 0 .25rem;
+  }
+
+  #navUp {
+    background-image: url("../../assets/folder.png");
+    background-size: 75%;
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 3rem;
+    height: 3rem;
+  }
+</style>
+
+<div class="nav">
+  <div class="icon-container" on:click={() => navUp}>
+    <i id="navUp" />
+    <p>UP</p>
+  </div>
+
+</div>
