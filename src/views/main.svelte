d10962e26fa77c81c1179d466cf9a5f7d328e72c
Mike Peiman <mikepeiman@gmail.com>
2020-11-19 11:12:58 -0500

    Installed svelte-tabs and implemented, works

--- src/views/main.svelte
+++ src/views/main.svelte
@@ -1,7 +1,26 @@
 <script>
   import FS from "./../filesystem/fs.svelte";
-  let selection = ''
+  import { Tabs, Tab, TabList, TabPanel } from "svelte-tabs";
+  let selection = "";
 </script>
 
 <h1>Main</h1>
-<FS />
+<Tabs>
+  <TabList>
+    <Tab>One</Tab>
+    <Tab>Two</Tab>
+    <Tab>Three</Tab>
+  </TabList>
+
+  <TabPanel>
+    <h2>Panel One</h2>
+  </TabPanel>
+
+  <TabPanel>
+    <FS />
+  </TabPanel>
+
+  <TabPanel>
+    <h2>Panel Three</h2>
+  </TabPanel>
+</Tabs>
